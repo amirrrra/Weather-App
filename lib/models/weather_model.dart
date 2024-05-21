74fc60862c8cc8db3fc491a74ed5f76574ade712
Amira Tarek <amirat737@gmail.com>
2024-05-21 15:06:05 +0300

    create weather model

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,31 @@
+class WeatherModel {
+  final String city;
+  final String date;
+  final String image;
+  final String state;
+  final double avgTemp;
+  final double maxTemp;
+  final double minTemp;
+
+  WeatherModel({
+    required this.city,
+    required this.date,
+    required this.image,
+    required this.state,
+    required this.avgTemp,
+    required this.maxTemp,
+    required this.minTemp,
+  });
+
+  factory WeatherModel.fromJson(json) {
+    return WeatherModel(
+      city: json['location']['name'],
+      date: json['current']['last_updated'],
+      image: json['current']['condition']['icon'],
+      state: json['current']['condition']['text'],
+      avgTemp: json['forecast']['forecastday'][0]['day']['avgtemp_c'],
+      maxTemp: json['forecast']['forecastday'][0]['day']['maxtemp_c'],
+      minTemp: json['forecast']['forecastday'][0]['day']['mintemp_c'],
+    );
+  }
+}
