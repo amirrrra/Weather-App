6876da1a3593fe98479ae5abda2f2be9f3c6ff9f
Amira Tarek <amirat737@gmail.com>
2024-05-22 01:47:00 +0300

    create get weather function

--- lib/cubits/get_weather_cubit/get_weather_cubit.dart
+++ lib/cubits/get_weather_cubit/get_weather_cubit.dart
@@ -1,6 +1,11 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/get_weather_cubit/get_weather_states.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
 
 class GetWeatherCubit extends Cubit<WeatherState> {
   GetWeatherCubit(super.initialState);
+  getWeather(String city) async {
+    WeatherModel weatherModel = await WeatherService().getWeather(city);
+  }
 }
