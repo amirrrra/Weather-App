67c80800af64bf5292f7ce3d7d258aae7f8e689b
Amira Tarek <amirat737@gmail.com>
2024-05-22 03:48:53 +0300

    integrate get weather cubit

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/models/weather_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_states.dart';
 import 'package:weather_app/widgets/app_bar_widget.dart';
 import 'package:weather_app/widgets/no_weather_data_widget.dart';
 import 'package:weather_app/widgets/weather_data_widget.dart';
@@ -11,9 +13,17 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const AppBarWidget(),
-      body: weatherModel == null
-          ? const NoWeatherDataWidget()
-          : const WeatherDataWidget(),
+      body: BlocBuilder<GetWeatherCubit, GetWeatherState>(
+        builder: (context, state) {
+          if (state is GetWeatherInitialState) {
+            return const NoWeatherDataWidget();
+          } else if (state is GetWeatherSuccessState) {
+            return const WeatherDataWidget();
+          } else {
+            return const Text('oops, there was an error');
+          }
+        },
+      ),
     );
   }
 }
